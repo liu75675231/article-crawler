847b073da6e964f9773f41f750de8a8c95b78217
liu75675231 <liu75675231@126.com>
2021-02-27 19:04:00 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ const {run, setReqConf} = require('./crawler.ts');
 const crawlerConf = require('./conf.ts');
 const fetch = require('node-fetch');
 
-fetch("https://developers.google.com/web/updates/rss.xml", {
-    "headers": {
-        "upgrade-insecure-requests": "1"
-    },
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": null,
-    "method": "GET",
-    "mode": "cors"
-}).then(res => res.text())
-    .then(body => console.log(body));;
+// fetch("https://developers.google.com/web/updates/rss.xml", {
+//     "headers": {
+//         "upgrade-insecure-requests": "1"
+//     },
+//     "referrerPolicy": "strict-origin-when-cross-origin",
+//     "body": null,
+//     "method": "GET",
+//     "mode": "cors"
+// }).then(res => res.text())
+//     .then(body => console.log(body));;
 
 //return;
 const dayjs = require('dayjs');
